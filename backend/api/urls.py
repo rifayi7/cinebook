57d8281e8ab79bf9a3190552ec39b05bfdba8c69
rifayi <rifayiavayil@gmail.com>
2026-01-07 15:20:26 +0530

    added basic access without  to  get,put,patch users  for admin

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from django import views
-from .views import Users,User,ActivateAccount
+from .views import Users,User,ActivateAccount,UserListForAdmin,SingleUserListForAdmin
 
 urlpatterns = [
     path('users',Users.as_view(),name="users"),
     path('user/<int:pk>',User.as_view(),name="user"),
+    path('admin/users',UserListForAdmin.as_view(),name="users-for-admin"),
+    path('admin/user/<int:pk>',SingleUserListForAdmin.as_view(),name="single-user-for-admin"),
     path("activate/<uidb64>/<token>/",ActivateAccount.as_view(),name="activate-account"),
+
 
     # path('create',create_user,name="createUser")
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import RifUser
-from .serializers import UserSerializer
+from .serializers import UserSerializer,AdminUserSerializer
 from rest_framework import status
 
 
@@ -108,3 +108,37 @@ class User(APIView):
 
 
 
+class SingleUserListForAdmin(APIView):
+    def get(self,request,pk):
+        user = RifUser.objects.get(id=pk)
+        serializer = AdminUserSerializer(user)
+        return Response(serializer.data)
+    def put(self,request,pk):
+        try:
+            user = RifUser.objects.get(id=pk)
+        except RifUser.DoesNotExist:
+            return Response({"detail": "User not found"}, status=404)
+        serializer = AdminUserSerializer(user,data=request.data,partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status=200)
+        return Response(serializer.errors, status=400)
+    def patch(self, request, pk):
+        try:
+            user = RifUser.objects.get(id=pk)
+        except RifUser.DoesNotExist:
+            return Response({"detail": "User not found"}, status=404)
+
+        serializer = AdminUserSerializer(user, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=200)
+
+        return Response(serializer.errors, status=400)
+class UserListForAdmin(APIView):
+    def get(self,request):
+        users = RifUser.objects.all()
+        serializer = AdminUserSerializer(users,many=True)
+        return Response(serializer.data)
+    
